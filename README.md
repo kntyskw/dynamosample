077b36691100d79520d9555bc3317bb2aed2f32a
EC2 Default User <ec2-user@ip-10-146-29-251.ap-northeast-1.compute.internal>
2012-10-03 04:12:34 +0000

    minor change

--- README.md
+++ README.md
@@ -4,9 +4,3 @@ dynamosocial
 1. Deploy on a PHP-enabled web server
 2. Run composer.sh or equivalent commands
 3. Copy common-sample.inc.php to common.inc.php and edit according to your environment
-
-To get Facebook event notifications
-
-1. Create Facebook application
-2. Set $fb_verify_token parameter in common.inc.php (any string which is hard to guess is fine)
-3. Create subscription for user feeds (See Facebook Realtime API guide: http://developers.facebook.com/docs/reference/api/realtime/) 
--- common-sample.inc.php
+++ common-sample.inc.php
@@ -1,22 +1,14 @@
 <?php 
 
-$appId = 'Your Facebook App Id';
-$appSecret = 'Your Facebook App Secret';
-$fb_verify_token = 'Your Facebook Realtime API callback verify token';
-
 $twitterConsumerKey = 'Your Twitter Consumer Key';
 $twitterConsumerSecret = 'Your Twitter Consumer Secret';
-
-$aws_access_key = 'Your AWS Access Key';
-$aws_secret_key = 'Your AWS Secret Key';
 
 $myUrl = 'http://your_domain/';
 $myWsUrl = 'http://your_websocket_domain/';
 
-$redis_server = 'Your Redis host name';
+$redis_server = 'localhost';
 $redis_port = '6379';
 
 require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/dbhelper.php';
 
 ?>
