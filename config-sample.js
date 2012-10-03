66ebf45c18e4c6821132f9a4fca465439e3dbbb5
root <root@ip-10-146-29-251.ap-northeast-1.compute.internal>
2012-10-03 06:57:16 +0000

    Updated streamer

---
+++ config-sample.js
@@ -0,0 +1,14 @@
+var config = exports;
+
+config.ws_port = 10080;
+config.aws_access_key = "AWS_ACCESS_KEY";
+config.aws_secret_key = "AWS_SECRET_KEY";
+config.redis_host = "localhost";
+config.redis_port = 6379;
+config.twitter_consumer_key = "TWITTER_OAUTH_CONSUMER_KEY";
+config.twitter_consumer_secret = "TWITTER_OAUTH_CONSUMER_SECRET";
+config.twitter_access_token = null;
+config.twitter_access_token_secret = null;
+
+
+
