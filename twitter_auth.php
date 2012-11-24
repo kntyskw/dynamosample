c36f5815a3a2cc4726563b350731071c98f47701
Kenta Yasukawa <kentay@amazon.com>
2012-11-24 13:27:37 +0900

    fixed dependency

--- twitter_auth.php
+++ twitter_auth.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/common.inc.php';
 require_once __DIR__.'/dbhelper.php';
-require_once __DIR__.'/vendor/inori/twitteroauth/twitteroauth/TwitterOAuth.php';
+require_once __DIR__.'/vendor/abraham/twitteroauth/twitteroauth/TwitterOAuth.php';
 session_start();
 
 
